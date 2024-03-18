7128c0e098e64aacbe5fe0d96ce5d6762e93107d
Mauro Corrales <mauro.corrales@crowdaronline.com>
2024-03-18 18:32:48 -0300

    get and set fixed_cost

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("fixed_cost_list", views.fixed_cost_list, name="fixed_cost_list"),
+    path("monthly", views.get_fixed_costs, name="fixed_cost_list"),
+    path('set_fixed_cost', views.set_fixed_cost, name='set_fixed_cost'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,20 +4,35 @@ from .forms import FixedCostForm
 from .models import FixedCost
 
 
-def get_fixed_cost(request):
-    form = FixedCostForm()
-    return render(request, 'monthly.html', {'form': form})
+def get_fixed_costs(request):
+    fixed_costs = FixedCost.objects.all()
+    return render(request, 'monthly.html', {'fixed_costs': fixed_costs})
 
 
 def set_fixed_cost(request):
     if request.method == 'POST':
         form = FixedCostForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('fixed_cost_list')
+            new_fixed_cost = form.save(commit=False)
+            existing_fixed_cost = FixedCost.objects.filter(month=new_fixed_cost.month, name=new_fixed_cost.name).first()
+            if existing_fixed_cost:
+                print("Si existe")
+                existing_fixed_cost.price = new_fixed_cost.price
+                for month in range(existing_fixed_cost.month, 13):
+                    FixedCost.objects.update_or_create(
+                        month=month,
+                        name=existing_fixed_cost.name,
+                        defaults={'price': existing_fixed_cost.price}
+                    )
+            else:
+                print("No existe")
+                for month in range(new_fixed_cost.month, 13):
+                    FixedCost.objects.update_or_create(
+                        month=month,
+                        name=new_fixed_cost.name,
+                        price=new_fixed_cost.price
+                    )
+            return redirect(get_fixed_costs)
+    else:
+        form = FixedCostForm()
     return render(request, 'monthly.html', {'form': form})
-
-
-def fixed_cost_list(request):
-    fixed_cost = FixedCost.objects.all()
-    return render(request, 'monthly.html', {'Fixed cost': fixed_cost})
