0f50ae0fceb11cd52e153474e3b516e99c65166d
Mauro Corrales <mauro.corrales@fravega.com.ar>
2024-11-12 20:50:11 -0300

    test cors

--- mysite/settings.py
+++ mysite/settings.py
@@ -59,6 +59,11 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware'
 ]
 
+CORS_ALLOW_HEADERS = [
+    "Authorization",
+    "Content-Type",
+]
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://maurocor.github.io"
