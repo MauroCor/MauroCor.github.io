53496b8626660d41af1233032fe487579ed044b6
Mauro Corrales <mauro.corrales@fravega.com.ar>
2024-11-13 14:35:15 -0300

    add login

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from .views import CardSpendListView, FixedCostListView, IncomeListView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/fixed-cost/', FixedCostListView.as_view(), name='fixed_cost'),
     path('api/income/', IncomeListView.as_view(), name='income'),
     path('api/card-spend/', CardSpendListView.as_view(), name='card_spend'),
--- mysite/settings.py
+++ mysite/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+from datetime import timedelta
 import os
 from pathlib import Path
 
@@ -44,7 +45,27 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'app',
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders'
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
 ]
 
 MIDDLEWARE = [
