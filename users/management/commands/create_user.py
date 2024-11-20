8ed38d4f6203a303a89b5ef7c912a24d3c7e0e5c
Mauro Corrales <mauro.corrales@fravega.com.ar>
2024-11-20 10:07:02 -0300

    fix create user command

--- users/management/commands/create_user.py
+++ users/management/commands/create_user.py
@@ -3,12 +3,26 @@ from django.contrib.auth.models import User
 from getpass import getpass
 
 class Command(BaseCommand):
-    help = 'Crea un usuario normal'
+    help = 'Crea un usuario normal con nombre y apellido'
 
     def handle(self, *args, **kwargs):
-        username = input('Ingresa user: ')
-        password = getpass('Ingresa password: ')
+        # Solicitar el nombre de usuario
+        username = input('Ingresa el nombre de usuario: ')
         
-        User.objects.create_user(username=username, email='test@a.com', password=password)
+        # Solicitar el nombre completo
+        first_name = input('Ingresa el nombre: ')
+        last_name = input('Ingresa el apellido: ')
         
-        self.stdout.write(self.style.SUCCESS(f'Usuario {username} creado exitosamente'))
+        # Solicitar la contraseña
+        password = getpass('Ingresa la contraseña: ')
+        
+        # Crear el usuario con los datos proporcionados
+        User.objects.create_user(
+            username=username,
+            email='test@a.com', 
+            password=password,
+            first_name=first_name,
+            last_name=last_name
+        )
+        
+        self.stdout.write(self.style.SUCCESS(f'Usuario {username} creado exitosamente con nombre {first_name} {last_name}.'))
